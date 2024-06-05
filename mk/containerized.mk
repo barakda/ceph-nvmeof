a132d0319fb05f40fe299b2fe2cdf9630f627b9b
barakda <barak.davidov@gmail.com>
2024-06-05 17:59:58 +0300

    push to private registry
    Signed-off-by: barakda <barak.davidov@gmail.com>

--- mk/containerized.mk
+++ mk/containerized.mk
@@ -24,17 +24,18 @@ build: DOCKER_COMPOSE_ENV = DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1
 
 push: ## Push nvmeof and nvmeof-cli containers images to quay.io registries
 	sudo bash -c 'echo $(QUAY)'
-	# SHORT_VERSION=$(shell echo $(VERSION) | cut -d. -f1-2); \
-	# docker push $(QUAY_NVMEOF):$(VERSION); \
-	# docker push $(QUAY_NVMEOFCLI):$(VERSION); \
-	# docker tag $(QUAY_NVMEOF):$(VERSION) $(QUAY_NVMEOF):$$SHORT_VERSION; \
-	# docker tag $(QUAY_NVMEOFCLI):$(VERSION) $(QUAY_NVMEOFCLI):$$SHORT_VERSION; \
-	# docker tag $(QUAY_NVMEOF):$(VERSION) $(QUAY_NVMEOF):latest; \
-	# docker tag $(QUAY_NVMEOFCLI):$(VERSION) $(QUAY_NVMEOFCLI):latest; \
-	# docker push $(QUAY_NVMEOF):$$SHORT_VERSION; \
-	# docker push $(QUAY_NVMEOFCLI):$$SHORT_VERSION; \
-	# docker push $(QUAY_NVMEOF):latest; \
-	# docker push $(QUAY_NVMEOFCLI):latest
+	QUAY_NVMEOF='$(QUAY)/nvmeof'
+	QUAY_NVMEOFCLI='$(QUAY)/nvmeof-cli'
+	docker push $(QUAY_NVMEOF):$(VERSION); \
+	docker push $(QUAY_NVMEOFCLI):$(VERSION); \
+	docker tag $(QUAY_NVMEOF):$(VERSION) $(QUAY_NVMEOF):$$SHORT_VERSION; \
+	docker tag $(QUAY_NVMEOFCLI):$(VERSION) $(QUAY_NVMEOFCLI):$$SHORT_VERSION; \
+	docker tag $(QUAY_NVMEOF):$(VERSION) $(QUAY_NVMEOF):latest; \
+	docker tag $(QUAY_NVMEOFCLI):$(VERSION) $(QUAY_NVMEOFCLI):latest; \
+	docker push $(QUAY_NVMEOF):$$SHORT_VERSION; \
+	docker push $(QUAY_NVMEOFCLI):$$SHORT_VERSION; \
+	docker push $(QUAY_NVMEOF):latest; \
+	docker push $(QUAY_NVMEOFCLI):latest
 
 run: ## Run command CMD inside SVC containers
 run: override OPTS += --rm
