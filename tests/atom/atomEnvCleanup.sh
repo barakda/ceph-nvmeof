aeca830f615fb31d0feaa3a1363c5fd61d62d784
barakda <barak.davidov@gmail.com>
2024-07-31 09:39:04 +0300

    atom_runner_test
    Signed-off-by: barakda <barak.davidov@gmail.com>

--- tests/atom/atomEnvCleanup.sh
+++ tests/atom/atomEnvCleanup.sh
@@ -1,45 +1,47 @@
 #!/bin/bash
 
-ATOM_IMAGE=0.0.9946
-VM_PASS=$1
+echo "HELLO BARAK"
 
-# echo $VM_PASS | sudo -S pwd;ls -lta
+# ATOM_IMAGE=0.0.9946
+# VM_PASS=$1
 
-while true; do
-    if [ -f "/home/cephnvme/busyServer.txt" ]; then
-        echo "The server is busy with another github action job, please wait..."
-        sleep 90
-    else
-        echo "The server is available for use!"
-	touch /tmp/busyServer.txt
-        chmod +rx /home/cephnvme/busyServer.txt
-        break
-    fi
-done
+# # echo $VM_PASS | sudo -S pwd;ls -lta
 
-echo "atom cleanup script run."
+# while true; do
+#     if [ -f "/home/cephnvme/busyServer.txt" ]; then
+#         echo "The server is busy with another github action job, please wait..."
+#         sleep 90
+#     else
+#         echo "The server is available for use!"
+# 	touch /tmp/busyServer.txt
+#         chmod +rx /home/cephnvme/busyServer.txt
+#         break
+#     fi
+# done
 
-echo $VM_PASS | sudo -S docker run -v /root/.ssh:/root/.ssh quay.io/barakda1/nvmeof_atom:$ATOM_IMAGE ansible-playbook -i custom_inventory.ini cephnvmeof_remove_cluster.yaml --extra-vars 'SELECTED_ENV=multiIBMCloudServers_m2'
+# echo "atom cleanup script run."
 
-cleanup_containers() {
-    local command=$1
-    sudo $command ps -q | xargs -r sudo $command stop
-    sudo $command ps -q | xargs -r sudo $command rm -f
-    sudo yes | $command system prune -fa
-    $command ps
-    $command images
-}
-cleanup_containers docker
-cleanup_containers podman
+# echo $VM_PASS | sudo -S docker run -v /root/.ssh:/root/.ssh quay.io/barakda1/nvmeof_atom:$ATOM_IMAGE ansible-playbook -i custom_inventory.ini cephnvmeof_remove_cluster.yaml --extra-vars 'SELECTED_ENV=multiIBMCloudServers_m2'
 
-remote_cleanup() {
-    local host=$1
-    ssh -o StrictHostKeyChecking=no root@$host "sudo docker ps -q | xargs -r sudo docker stop; \
-    sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; \
-    sudo docker ps; sudo docker images"
-}
-hosts=("cephnvme-vm9" "cephnvme-vm7" "cephnvme-vm6" "cephnvme-vm1")
+# cleanup_containers() {
+#     local command=$1
+#     sudo $command ps -q | xargs -r sudo $command stop
+#     sudo $command ps -q | xargs -r sudo $command rm -f
+#     sudo yes | $command system prune -fa
+#     $command ps
+#     $command images
+# }
+# cleanup_containers docker
+# cleanup_containers podman
 
-for host in "${hosts[@]}"; do
-    remote_cleanup $host
-done
+# remote_cleanup() {
+#     local host=$1
+#     ssh -o StrictHostKeyChecking=no root@$host "sudo docker ps -q | xargs -r sudo docker stop; \
+#     sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; \
+#     sudo docker ps; sudo docker images"
+# }
+# hosts=("cephnvme-vm9" "cephnvme-vm7" "cephnvme-vm6" "cephnvme-vm1")
+
+# for host in "${hosts[@]}"; do
+#     remote_cleanup $host
+# done
