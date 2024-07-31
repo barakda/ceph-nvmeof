55c79250f88b2bf4fed101dbdd9469601baa9827
barakda <barak.davidov@gmail.com>
2024-07-31 16:45:13 +0300

    atom_runner_test
    Signed-off-by: barakda <barak.davidov@gmail.com>

---
+++ tests/atom/atomClusterBuildTestsRun.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ATOM_IMAGE='1.0.0'
+RUNNER_PASS=$1
+REPO=$2
+BRANCH=$3
+VERSION=$4
+CEPH_SHA=$5
+
+echo "=> The atom test is about to run with the next parameters:"
+echo "   NVMEoF repo: $REPO"
+echo "   Branch: $BRANCH"
+echo "   NVMEoF version: $VERSION"
+echo "   Ceph SHA: $CEPH_SHA"
+
+if [ $BRANCH = "merge" ]; then
+    REPO="ceph/ceph-nvmeof"
+    BRANCH="devel"
+fi
+
+echo "atom test script run:"
+echo $RUNNER_PASS | sudo -S docker run -v /root/.ssh:/root/.ssh nvmeof_atom:$ATOM_IMAGE python3 cephnvme_atom.py quay.ceph.io/ceph-ci/ceph:$CEPH_SHA quay.io/ceph/nvmeof:$VERSION quay.io/ceph/nvmeof-cli:$VERSION https://github.com/$REPO $BRANCH None None None 4 1 2 4 1024 2 2 200M 0 1 --stopNvmeofDaemon --stopNvmeofSystemctl --stopMonLeader --gitHubActionDeployment --dontUseMTLS --dontPowerOffCloudVMs noKey --multiIBMCloudServers_m2
---
+++ tests/atom/atomEnvInit.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+ATOM_IMAGE=1.0.0
+ATOM_REPO_OWNER=$1
+NVMEOF_REPO_OWNER=$2
+ATOM_REPO_TOKEN=$3
+RUNNER_PASS=$4
+
+
+pwd;ls -lta
+
+###??### rm -rf /home/cephnvme/actions-runner-barakda/brkd_work/ceph-nvmeof/ceph-nvmeof/
+rm -rf /home/cephnvme/actions-runner-$NVMEOF_REPO_OWNER/ceph-nvmeof-atom
+
+while true; do
+    if [ -f "/home/cephnvme/busyServer.txt" ]; then
+        echo "The server is busy with another github action job, please wait..."
+        sleep 90
+    else
+        echo "The server is available for use!"
+	    touch /home/cephnvme/busyServer.txt
+        chmod +rx /home/cephnvme/busyServer.txt
+        break
+    fi
+done
+
+TRIMMED_ATOM_REPO_OWNER="${ATOM_REPO_OWNER%?}"
+
+echo "=> Cleanup docker images:"
+echo $RUNNER_PASS | sudo -S sh -c 'docker ps -q | xargs -r sudo docker stop; sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; docker ps; docker images'
+
+echo "=> Cloning atom repo:"
+git clone --branch devel https://$TRIMMED_ATOM_REPO_OWNER:$ATOM_REPO_TOKEN@github.ibm.com/NVME-Over-Fiber/ceph-nvmeof-atom.git /home/cephnvme/actions-runner-$NVMEOF_REPO_OWNER/ceph-nvmeof-atom
+echo "=> Build atom images based on the cloned repo:"
+docker build -t nvmeof_atom:$ATOM_IMAGE /home/cephnvme/actions-runner-$NVMEOF_REPO_OWNER/ceph-nvmeof-atom
+echo "=> Remove ceph cluster:"
+docker run -v /root/.ssh:/root/.ssh nvmeof_atom:$ATOM_IMAGE ansible-playbook -i custom_inventory.ini cephnvmeof_remove_cluster.yaml --extra-vars 'SELECTED_ENV=multiIBMCloudServers_m2'
+
+echo "Cleanup remain ceph images:"
+ssh -o StrictHostKeyChecking=no root@cephnvme-vm9 'sudo docker ps -q | xargs -r sudo docker stop; sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; sudo docker ps; sudo docker images'
+ssh -o StrictHostKeyChecking=no root@cephnvme-vm7 'sudo docker ps -q | xargs -r sudo docker stop; sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; sudo docker ps; sudo docker images'
+ssh -o StrictHostKeyChecking=no root@cephnvme-vm6 'sudo docker ps -q | xargs -r sudo docker stop; sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; sudo docker ps; sudo docker images'
+ssh -o StrictHostKeyChecking=no root@cephnvme-vm1 'sudo docker ps -q | xargs -r sudo docker stop; sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; sudo docker ps; sudo docker images'
+
+echo $RUNNER_PASS | sudo -S sh -c 'podman ps -q | xargs -r sudo podman stop; sudo podman ps -q | xargs -r sudo podman rm -f; sudo yes | podman system prune -fa; podman ps; podman images'
