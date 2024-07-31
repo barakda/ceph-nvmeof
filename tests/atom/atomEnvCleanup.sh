275cc32d64ec7c3438d77732e384f7fa3ea3616a
barakda <barak.davidov@gmail.com>
2024-07-31 12:08:45 +0300

    atom_runner_test
    Signed-off-by: barakda <barak.davidov@gmail.com>

--- tests/atom/atomEnvCleanup.sh
+++ tests/atom/atomEnvCleanup.sh
@@ -6,13 +6,14 @@ RUNNER_PASS=$3
 ATOM_IMAGE=latest
 
 pwd;ls -lta
-echo $RUNNER_PASS | sudo -S pwd;ls -lta
+# echo $RUNNER_PASS | sudo -S pwd;ls -lta
 
 TRIMMED_ATOM_REPO_OWNER="${ATOM_REPO_OWNER%?}"
 
-echo "cloning atom repo"
-git clone --branch devel https://$TRIMMED_ATOM_REPO_OWNER:$ATOM_REPO_TOKEN@github.ibm.com/NVME-Over-Fiber/ceph-nvmeof-atom.git /home/cephnvme/
+echo "Cloning atom repo:"
+git clone --branch devel https://$TRIMMED_ATOM_REPO_OWNER:$ATOM_REPO_TOKEN@github.ibm.com/NVME-Over-Fiber/ceph-nvmeof-atom.git /home/cephnvme/ceph-nvmeof-atom
 
+docker build -t quay.io/@ATOM_REPO_OWNER/nvmeof_atom:test1 /home/cephnvme/ceph-nvmeof-atom
 
 while true; do
     if [ -f "/home/cephnvme/busyServer.txt" ]; then
@@ -20,15 +21,11 @@ while true; do
         sleep 90
     else
         echo "The server is available for use!"
-	touch /tmp/busyServer.txt
+	    touch /home/cephnvme/busyServer.txt
         chmod +rx /home/cephnvme/busyServer.txt
         break
     fi
 done
-
-echo "atom cleanup script run."
-
-echo $RUNNER_PASS | sudo -S docker run -v /root/.ssh:/root/.ssh quay.io/barakda1/nvmeof_atom:$ATOM_IMAGE ansible-playbook -i custom_inventory.ini cephnvmeof_remove_cluster.yaml --extra-vars 'SELECTED_ENV=multiIBMCloudServers_m2'
 
 cleanup_containers() {
     local command=$1
@@ -52,3 +49,5 @@ hosts=("cephnvme-vm9" "cephnvme-vm7" "cephnvme-vm6" "cephnvme-vm1")
 for host in "${hosts[@]}"; do
     remote_cleanup $host
 done
+
+echo $RUNNER_PASS | sudo -S docker run -v /root/.ssh:/root/.ssh quay.io/barakda1/nvmeof_atom:$ATOM_IMAGE ansible-playbook -i custom_inventory.ini cephnvmeof_remove_cluster.yaml --extra-vars 'SELECTED_ENV=multiIBMCloudServers_m2'
