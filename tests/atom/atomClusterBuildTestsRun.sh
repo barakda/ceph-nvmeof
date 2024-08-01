4df7b780d468519bf89e51fa794b58452eb1f0ee
barakda <barak.davidov@gmail.com>
2024-08-01 10:22:51 +0300

    atom_runner_test
    Signed-off-by: barakda <barak.davidov@gmail.com>

--- tests/atom/atomClusterBuildTestsRun.sh
+++ tests/atom/atomClusterBuildTestsRun.sh
@@ -7,16 +7,27 @@ VERSION=$4
 CEPH_SHA=$5
 ATOM_SHA=$6
 
-echo "=> The atom test is about to run with the next parameters:"
-echo "   NVMEoF repo: $REPO"
-echo "   Branch: $BRANCH"
-echo "   NVMEoF version: $VERSION"
-echo "   Ceph SHA: $CEPH_SHA"
-
 if [ $BRANCH = "merge" ]; then
     REPO="ceph/ceph-nvmeof"
     BRANCH="devel"
 fi
 
-echo "atom test script run:"
-echo $RUNNER_PASS | sudo -S docker run -v /root/.ssh:/root/.ssh nvmeof_atom:$ATOM_SHA python3 cephnvme_atom.py quay.ceph.io/ceph-ci/ceph:$CEPH_SHA quay.io/ceph/nvmeof:$VERSION quay.io/ceph/nvmeof-cli:$VERSION https://github.com/$REPO $BRANCH None None None 4 1 2 4 1024 2 2 200M 0 1 --stopNvmeofDaemon --stopNvmeofSystemctl --stopMonLeader --gitHubActionDeployment --dontUseMTLS --dontPowerOffCloudVMs noKey --multiIBMCloudServers_m2
+echo "=> atom test script run:"
+# echo $RUNNER_PASS | sudo -S docker run -v /root/.ssh:/root/.ssh nvmeof_atom:$ATOM_SHA python3 cephnvme_atom.py quay.ceph.io/ceph-ci/ceph:$CEPH_SHA quay.io/ceph/nvmeof:$VERSION quay.io/ceph/nvmeof-cli:$VERSION https://github.com/$REPO $BRANCH None None None 4 1 2 4 1024 2 2 200M 0 1 --stopNvmeofDaemon --stopNvmeofSystemctl --stopMonLeader --gitHubActionDeployment --dontUseMTLS --dontPowerOffCloudVMs noKey --multiIBMCloudServers_m2
+echo "$RUNNER_PASS" | sudo -S docker run \
+    -v /root/.ssh:/root/.ssh \
+    nvmeof_atom:"$ATOM_SHA" \
+    python3 cephnvme_atom.py \
+    quay.ceph.io/ceph-ci/ceph:"$CEPH_SHA" \
+    quay.io/ceph/nvmeof:"$VERSION" \
+    quay.io/ceph/nvmeof-cli:"$VERSION" \
+    "https://github.com/$REPO" \
+    "$BRANCH" \
+    None None None 4 1 2 4 1024 2 2 200M 0 1 \
+    --stopNvmeofDaemon \
+    --stopNvmeofSystemctl \
+    --stopMonLeader \
+    --gitHubActionDeployment \
+    --dontUseMTLS \
+    --dontPowerOffCloudVMs noKey \
+    --multiIBMCloudServers_m2
--- tests/atom/atomEnvInit.sh
+++ tests/atom/atomEnvInit.sh
@@ -7,8 +7,23 @@ RUNNER_PASS=$4
 ATOM_BRANCH=$5
 ATOM_SHA=$6
 
-pwd;ls -lta
+TRIMMED_ATOM_REPO_OWNER="${ATOM_REPO_OWNER%?}"
+
+cleanup_docker_images() {
+    local HOST=$1
+    ssh -o StrictHostKeyChecking=no root@$HOST << EOF
+    sudo docker ps -q | xargs -r sudo docker stop
+    sudo docker ps -q | xargs -r sudo docker rm -f
+    sudo yes | sudo docker system prune -fa
+    sudo docker ps
+    sudo docker images
+EOF
+}
+
+# Remove repo folder
 rm -rf /home/cephnvme/actions-runner-$NVMEOF_REPO_OWNER/ceph-nvmeof-atom
+
+# Check if cluster is busy with another run
 while true; do
     if [ -f "/home/cephnvme/busyServer.txt" ]; then
         echo "The server is busy with another github action job, please wait..."
@@ -21,25 +36,50 @@ while true; do
     fi
 done
 
-TRIMMED_ATOM_REPO_OWNER="${ATOM_REPO_OWNER%?}"
-
-echo "=> Cleanup docker images:"
+# Cleanup docker images
 echo $RUNNER_PASS | sudo -S sh -c 'docker ps -q | xargs -r sudo docker stop; sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; docker ps; docker images'
 
-echo "=> Cloning atom repo:"
+# Cloning atom repo
 git clone --branch $ATOM_BRANCH https://$TRIMMED_ATOM_REPO_OWNER:$ATOM_REPO_TOKEN@github.ibm.com/NVME-Over-Fiber/ceph-nvmeof-atom.git /home/cephnvme/actions-runner-$NVMEOF_REPO_OWNER/ceph-nvmeof-atom
-echo "=> Switch to given SHA:"
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to clone the atom repository."
+    exit 1
+fi
+
+# Switch to given SHA
 cd /home/cephnvme/actions-runner-$NVMEOF_REPO_OWNER/ceph-nvmeof-atom
 git checkout $ATOM_SHA
-echo "=> Build atom images based on the cloned repo:"
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to checkout the specified SHA."
+    exit 1
+fi
+
+# Build atom images based on the cloned repo
 docker build -t nvmeof_atom:$ATOM_SHA /home/cephnvme/actions-runner-$NVMEOF_REPO_OWNER/ceph-nvmeof-atom
-echo "=> Remove ceph cluster:"
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to build Docker image."
+    exit 1
+fi
+
+# Remove ceph cluster
 docker run -v /root/.ssh:/root/.ssh nvmeof_atom:$ATOM_SHA ansible-playbook -i custom_inventory.ini cephnvmeof_remove_cluster.yaml --extra-vars 'SELECTED_ENV=multiIBMCloudServers_m2'
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to run cephnvmeof_remove_cluster ansible-playbook."
+    exit 1
+fi
 
-echo "Cleanup remain ceph images:"
-ssh -o StrictHostKeyChecking=no root@cephnvme-vm9 'sudo docker ps -q | xargs -r sudo docker stop; sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; sudo docker ps; sudo docker images'
-ssh -o StrictHostKeyChecking=no root@cephnvme-vm7 'sudo docker ps -q | xargs -r sudo docker stop; sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; sudo docker ps; sudo docker images'
-ssh -o StrictHostKeyChecking=no root@cephnvme-vm6 'sudo docker ps -q | xargs -r sudo docker stop; sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; sudo docker ps; sudo docker images'
-ssh -o StrictHostKeyChecking=no root@cephnvme-vm1 'sudo docker ps -q | xargs -r sudo docker stop; sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; sudo docker ps; sudo docker images'
+echo "Cleanup remain images after ceph cluster removal:"
+HOSTS=("cephnvme-vm9" "cephnvme-vm7" "cephnvme-vm6" "cephnvme-vm1")
+for HOST in "${HOSTS[@]}"; do
+    echo "=> Cleaning up Docker images on $HOST"
+    cleanup_docker_images "$HOST"
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to clean up Docker images on $HOST."
+    fi
+done
 
+# ssh -o StrictHostKeyChecking=no root@cephnvme-vm9 'sudo docker ps -q | xargs -r sudo docker stop; sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; sudo docker ps; sudo docker images'
+# ssh -o StrictHostKeyChecking=no root@cephnvme-vm7 'sudo docker ps -q | xargs -r sudo docker stop; sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; sudo docker ps; sudo docker images'
+# ssh -o StrictHostKeyChecking=no root@cephnvme-vm6 'sudo docker ps -q | xargs -r sudo docker stop; sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; sudo docker ps; sudo docker images'
+# ssh -o StrictHostKeyChecking=no root@cephnvme-vm1 'sudo docker ps -q | xargs -r sudo docker stop; sudo docker ps -q | xargs -r sudo docker rm -f; sudo yes | docker system prune -fa; sudo docker ps; sudo docker images'
 echo $RUNNER_PASS | sudo -S sh -c 'podman ps -q | xargs -r sudo podman stop; sudo podman ps -q | xargs -r sudo podman rm -f; sudo yes | podman system prune -fa; podman ps; podman images'
