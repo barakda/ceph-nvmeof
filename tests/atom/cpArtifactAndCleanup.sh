ba14e0f5c3118d6e6e739eb7391a3000140c67f9
barakda <barak.davidov@gmail.com>
2024-08-02 11:30:42 +0300

    atom tests add
    Signed-off-by: barakda <barak.davidov@gmail.com>

--- tests/atom/cpArtifactAndCleanup.sh
+++ tests/atom/cpArtifactAndCleanup.sh
@@ -2,7 +2,7 @@
 
 RUNNER_PASS=$1
 
-echo $RUNNER_PASS | sudo -S cp -r /root/.ssh/atom_backup/artifact /tmp/
+echo $RUNNER_PASS | sudo -S cp -r /root/.ssh/atom_backup/artifact/multiIBMCloudServers_m2 /tmp/artifact
 sudo ls -lta /tmp/artifact
 sudo chmod -R +rx /tmp/artifact
 rm -rf /home/cephnvme/busyServer.txt
