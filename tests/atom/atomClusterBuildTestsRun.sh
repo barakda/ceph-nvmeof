acfca43360f388b55360e59ad9fdcbcca58ad0f4
barakda <barak.davidov@gmail.com>
2024-08-02 07:59:02 +0300

    atom_runner_test
    Signed-off-by: barakda <barak.davidov@gmail.com>

--- tests/atom/atomClusterBuildTestsRun.sh
+++ tests/atom/atomClusterBuildTestsRun.sh
@@ -12,44 +12,37 @@ if [ $BRANCH = "merge" ]; then
     BRANCH="devel"
 fi
 
-echo $RUNNER_PASS | sudo -S docker run -v /root/.ssh:/root/.ssh nvmeof_atom:$ATOM_SHA python3 cephnvme_atom.py quay.ceph.io/ceph-ci/ceph:$CEPH_SHA quay.io/ceph/nvmeof:$VERSION quay.io/ceph/nvmeof-cli:$VERSION https://github.com/$REPO $BRANCH None None None 4 1 2 4 1024 2 2 200M 0 1 --stopNvmeofDaemon --stopNvmeofSystemctl --stopMonLeader --gitHubActionDeployment --dontUseMTLS --dontPowerOffCloudVMs noKey --multiIBMCloudServers_m2
-
-
 # Atom test script run
 # Description of the uncleared flags with their default values
-# Upgrade ceph image target (None)
-# Upgrade nvmeof image target (None)
-# Nvmeof cli image use in target (None)
-# Number of gateways (4)
-# Number of gateways to stop (1)
-# Number of subsystems (2)
-# Number of namespaces (4)
-# Max namespaces per subsystem (1024)
-# HA failover cycles (2)
-# HA failover cycles after upgrade (2)
-# RBD size (200M)
-# Seed number (0)
-# FIO use (1=run fio, 0=don't run fio)
+#   Upgrade ceph image target (None)
+#   Upgrade nvmeof image target (None)
+#   Nvmeof cli image use in target (None)
+#   Number of gateways (4)
+#   Number of gateways to stop (1)
+#   Number of subsystems (2)
+#   Number of namespaces (4)
+#   Max namespaces per subsystem (1024)
+#   HA failover cycles (2)
+#   HA failover cycles after upgrade (2)
+#   RBD size (200M)
+#   Seed number (0)
+#   FIO use (1=run fio, 0=don't run fio)
+# echo $RUNNER_PASS | sudo -S docker run -v /root/.ssh:/root/.ssh nvmeof_atom:$ATOM_SHA python3 cephnvme_atom.py quay.ceph.io/ceph-ci/ceph:$CEPH_SHA quay.io/ceph/nvmeof:$VERSION quay.io/ceph/nvmeof-cli:$VERSION https://github.com/$REPO $BRANCH None None None 4 1 2 4 1024 2 2 200M 0 1 --stopNvmeofDaemon --stopNvmeofSystemctl --stopMonLeader --gitHubActionDeployment --dontUseMTLS --dontPowerOffCloudVMs noKey --multiIBMCloudServers_m2
 
-# echo "$RUNNER_PASS" | sudo -S docker run \
-#     -v /root/.ssh:/root/.ssh \
-#     nvmeof_atom:"$ATOM_SHA" \
-#     python3 cephnvme_atom.py \
-#     quay.ceph.io/ceph-ci/ceph:"$CEPH_SHA" \
-#     quay.io/ceph/nvmeof:"$VERSION" \
-#     quay.io/ceph/nvmeof-cli:"$VERSION" \
-#     "https://github.com/$REPO" \
-#     "$BRANCH" \
-#     None None None 4 1 2 4 1024 2 2 200M 0 1 \
-#     --stopNvmeofDaemon \
-#     --stopNvmeofSystemctl \
-#     --stopMonLeader \
-#     --gitHubActionDeployment \
-#     --dontUseMTLS \
-#     --dontPowerOffCloudVMs noKey \
-#     --multiIBMCloudServers_m2
-
- 
-
-
-
+echo "$RUNNER_PASS" | sudo -S docker run \
+    -v /root/.ssh:/root/.ssh \
+    nvmeof_atom:"$ATOM_SHA" \
+    python3 cephnvme_atom.py \
+    quay.ceph.io/ceph-ci/ceph:"$CEPH_SHA" \
+    quay.io/ceph/nvmeof:"$VERSION" \
+    quay.io/ceph/nvmeof-cli:"$VERSION" \
+    "https://github.com/$REPO" \
+    "$BRANCH" \
+    None None None 4 1 2 4 1024 2 2 200M 0 1 \
+    --stopNvmeofDaemon \
+    --stopNvmeofSystemctl \
+    --stopMonLeader \
+    --gitHubActionDeployment \
+    --dontUseMTLS \
+    --dontPowerOffCloudVMs noKey \
+    --multiIBMCloudServers_m2
