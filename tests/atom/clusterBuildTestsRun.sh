dac129dd04832eb528ff336526e63eec77cfe99a
barakda <barak.davidov@gmail.com>
2024-09-30 07:54:32 +0300

    atom_nightly_run
    Signed-off-by: barakda <barak.davidov@gmail.com>

--- tests/atom/clusterBuildTestsRun.sh
+++ tests/atom/clusterBuildTestsRun.sh
@@ -13,6 +13,8 @@ ATOM_SHA=$3
 #   - Upgrade ceph image target (None)
 #   - Upgrade nvmeof image target (None)
 #   - Nvmeof cli image use in target (None)
+#   - Initiator num (1)
+#   - Number of groups (1)
 #   - Number of gateways (4)
 #   - Number of gateways to stop (1)
 #   - Number of gateways after scale down (1)
@@ -24,6 +26,25 @@ ATOM_SHA=$3
 #   - RBD size (200M)
 #   - Seed number (0)
 #   - FIO use (1=run fio, 0=don't run fio)
+echo "sudo docker run \
+    -v /root/.ssh:/root/.ssh \
+    nvmeof_atom:"$ATOM_SHA" \
+    python3 cephnvme_atom.py \
+    quay.ceph.io/ceph-ci/ceph:"$CEPH_SHA" \
+    quay.io/ceph/nvmeof:"$VERSION" \
+    quay.io/ceph/nvmeof-cli:"$VERSION" \
+    None None None None None None 1 1 4 1 1 2 4 1024 2 2 200M 0 1 20 10 1 \
+    --stopNvmeofDaemon \
+    --stopNvmeofSystemctl \
+    --stopMonLeader \
+    --rmNvmeofDaemon \
+    --gitHubActionDeployment \
+    --dontUseMTLS \
+    --skiplbTest \
+    --journalctlToConsole \
+    --dontPowerOffCloudVMs noKey noKey \
+    --multiIBMCloudServers_m2"
+
 sudo docker run \
     -v /root/.ssh:/root/.ssh \
     nvmeof_atom:"$ATOM_SHA" \
@@ -31,7 +52,7 @@ sudo docker run \
     quay.ceph.io/ceph-ci/ceph:"$CEPH_SHA" \
     quay.io/ceph/nvmeof:"$VERSION" \
     quay.io/ceph/nvmeof-cli:"$VERSION" \
-    None None None None None None 4 1 1 2 4 1024 2 2 200M 0 1 20 20 1 \
+    None None None None None None 1 1 4 1 1 2 4 1024 2 2 200M 0 1 20 10 1 \
     --stopNvmeofDaemon \
     --stopNvmeofSystemctl \
     --stopMonLeader \
