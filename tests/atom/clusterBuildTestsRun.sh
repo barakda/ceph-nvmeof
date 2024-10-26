504d7d30baa2f6a5aadc61a0f98411f117aa1fe6
barakda <barak.davidov@gmail.com>
2024-10-26 21:43:53 +0300

    new_runner_check
    Signed-off-by: barakda <barak.davidov@gmail.com>

--- tests/atom/clusterBuildTestsRun.sh
+++ tests/atom/clusterBuildTestsRun.sh
@@ -24,6 +24,26 @@ ATOM_SHA=$3
 #   - RBD size (200M)
 #   - Seed number (0)
 #   - FIO use (1=run fio, 0=don't run fio)
+echo "sudo docker run \
+    -v /root/.ssh:/root/.ssh \
+    nvmeof_atom:"$ATOM_SHA" \
+    python3 cephnvme_atom.py \
+    quay.ceph.io/ceph-ci/ceph:"$CEPH_SHA" \
+    quay.io/ceph/nvmeof:"$VERSION" \
+    quay.io/ceph/nvmeof-cli:"$VERSION" \
+    None None None None None None 1 1 4 1 1 2 4 1024 2 2 200M 0 1 20 10 1 \
+    --stopNvmeofDaemon \
+    --stopNvmeofSystemctl \
+    --stopMonLeader \
+    --rmNvmeofDaemon \
+    --gitHubActionDeployment \
+    --dontUseMTLS \
+    --skiplbGroupChangeTest \
+    --skiplbTest \
+    --journalctlToConsole \
+    --dontPowerOffCloudVMs noKey noKey \
+    --multiIBMCloudServers_m6"
+
 sudo docker run \
     -v /root/.ssh:/root/.ssh \
     nvmeof_atom:"$ATOM_SHA" \
