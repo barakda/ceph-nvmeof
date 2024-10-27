17f74739b6d65efc96ba45b85ddb26abee8b9f11
barakda <barak.davidov@gmail.com>
2024-10-27 12:40:28 +0200

    new_runner_check_perf
    Signed-off-by: barakda <barak.davidov@gmail.com>

--- tests/atom/cpArtifactAndCleanup.sh
+++ tests/atom/cpArtifactAndCleanup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-sudo rm -rf /tmp/artifact/multiIBMCloudServers_m6
-sudo cp -r /root/.ssh/atom_backup/artifact/multiIBMCloudServers_m6 /tmp/artifact
-sudo ls -lta /tmp/artifact
-sudo chmod -R +rx /tmp/artifact
+sudo rm -rf /home/cephnvme/artifact/multiIBMCloudServers_m6
+sudo cp -r /root/.ssh/atom_backup/artifact/multiIBMCloudServers_m6 /home/cephnvme/artifact
+sudo chown -R cephnvme:cephnvme /home/cephnvme/artifact
+tar -czf /home/cephnvme/artifact.tar.gz -C /home/cephnvme/artifact .
+ls -lta /home/cephadm/artifact
+ls -lta /home/cephadm
+chmod +rx /home/cephnvme/artifact.tar.gz
 rm -rf /home/cephnvme/busyServer.txt
