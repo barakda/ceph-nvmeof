143de364326081c2942509a70b83495faaccb1bb
barakda <barak.davidov@gmail.com>
2024-10-27 22:43:29 +0200

    new_runner_check_artifact
    Signed-off-by: barakda <barak.davidov@gmail.com>

--- tests/atom/cpArtifactAndCleanup.sh
+++ tests/atom/cpArtifactAndCleanup.sh
@@ -1,23 +1,17 @@
 !/bin/bash -x
 
-sudo rm -rf /tmp/artifact/multiIBMCloudServers_m6
-sudo cp -r /root/.ssh/atom_backup/artifact/multiIBMCloudServers_m6 /tmp/artifact
-sudo ls -lta /tmp/artifact
-sudo chmod -R +rx /tmp/artifact
-rm -rf /home/cephnvme/busyServer.txt
+sudo rm -rf /home/cephnvme/artifact/*
+sudo ls -lta /home/cephnvme/artifact
 
-# sudo rm -rf /home/cephnvme/artifact/*
-# sudo ls -lta /home/cephnvme/artifact
+sudo rm -rf /home/cephnvme/artifact.tar.gz
+sudo ls -lta /home/cephnvme/
 
-# sudo rm -rf /home/cephnvme/artifact.tar.gz
-# sudo ls -lta /home/cephnvme/
+sudo cp -r /root/.ssh/atom_backup/artifact/multiIBMCloudServers_m6 /home/cephnvme/artifact
+# sudo chown -R cephnvme:cephnvme /home/cephnvme/artifact
+sudo ls -lta /home/cephnvme/artifact
 
-# sudo cp -r /root/.ssh/atom_backup/artifact/multiIBMCloudServers_m6 /home/cephnvme/artifact
-# # sudo chown -R cephnvme:cephnvme /home/cephnvme/artifact
-# sudo ls -lta /home/cephnvme/artifact
-
-# # sudo tar -czf /home/cephnvme/artifact.tar.gz -C /home/cephnvme/artifact .
-# sudo ls -lta /home/cephnvme/artifact
-# sudo ls -lta /home/cephnvme
-# # sudo chmod +rx /home/cephnvme/artifact.tar.gz
-# sudo rm -rf /home/cephnvme/busyServer.txt
+# sudo tar -czf /home/cephnvme/artifact.tar.gz -C /home/cephnvme/artifact .
+sudo ls -lta /home/cephnvme/artifact
+sudo ls -lta /home/cephnvme
+# sudo chmod +rx /home/cephnvme/artifact.tar.gz
+sudo rm -rf /home/cephnvme/busyServer.txt
